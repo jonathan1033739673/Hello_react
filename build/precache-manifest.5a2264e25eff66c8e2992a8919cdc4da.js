a45f9ae6cfde3273082d8d745d1383c383ea5c31
Jonathan Gonzalez <jonathan.gonzalez-b@outlook.com>
2020-11-24 07:00:18 +1000

    create a build folder to be uploaded on netlify

---
+++ build/precache-manifest.5a2264e25eff66c8e2992a8919cdc4da.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0359309774a155f72ab7fc14f0bc80fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c380bc25c4e6a186d1a",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "d61d86752748685de703",
+    "url": "/static/css/main.f111d659.chunk.css"
+  },
+  {
+    "revision": "1c380bc25c4e6a186d1a",
+    "url": "/static/js/2.3d9d821e.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/static/js/2.3d9d821e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d61d86752748685de703",
+    "url": "/static/js/main.2da2b02f.chunk.js"
+  },
+  {
+    "revision": "9f2bccb44838f31c11fb",
+    "url": "/static/js/runtime-main.e1722758.js"
+  },
+  {
+    "revision": "71addf454967249472471f5398055e3d",
+    "url": "/static/media/astronauts.71addf45.svg"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "a1003ad867327b9b2ce3f8a467158f4a",
+    "url": "/static/media/img404.a1003ad8.png"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/static/media/stars.1d17360a.svg"
+  }
+]);
